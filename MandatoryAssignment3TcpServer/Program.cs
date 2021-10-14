1e8fba7583dc7e019b28ba77ef2ebd5c75548931
george <geor2022@edu.easj.dk>
2021-10-14 17:52:37 +0200

    Add project files.

---
+++ MandatoryAssignment3TcpServer/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using MandatoryAssignment2RestService;
+using MandatoryAssigment1Library;
+using System.Threading.Tasks;
+
+namespace MandatoryAssignment3TcpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 4646);
+            listener.Start();
+            while (true)
+            {
+                Console.WriteLine("Server ready");
+                TcpClient socket = listener.AcceptTcpClient();
+                Console.WriteLine("Incoming client");
+                Task.Run(() =>
+                {
+                    DoClient(socket);
+                });
+            }
+        }
+        private static void DoClient(TcpClient socket)
+        {
+            NetworkStream ns = socket.GetStream();
+            StreamReader reader = new StreamReader(ns);
+            StreamWriter writer = new StreamWriter(ns);
+            RestClient slave = new RestClient();
+            string rmessage;
+            while (true)
+            {
+                string message1 = reader.ReadLine().ToLower();
+                string message2 = reader.ReadLine().ToLower();
+                switch (message1)
+                {
+                    case "get":
+                        rmessage = slave.Get(message2).Result;
+                        writer.WriteLine(rmessage);
+                        writer.Flush();
+                        break;
+                    case "getall":
+                        rmessage = slave.GetAll().Result;
+                        writer.WriteLine(rmessage);
+                        writer.Flush();
+                        break;
+                    case "post":
+                        _ = slave.Post(message2);
+                        break;
+                    case "put":
+                        _ = slave.Put(message2);
+                        break;
+                    case "delete":
+                        slave.Remove(message2);
+                        break;
+                    case "help":
+                        break;
+                    case "close":
+                        writer.Write("Server closed connection");
+                        writer.Flush();
+                        socket.Close();
+                        break;
+                    default:
+                        break;
+                }
+                if (message1 == "close") break;
+            }
+
+        }
+    }
+}
---
+++ MandatoryAssignment3TcpServer/RestClient.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using MandatoryAssigment1Library;
+using Newtonsoft.Json;
+
+namespace MandatoryAssignment3TcpServer
+{
+    public class RestClient
+    {
+        public async Task<string> GetAll()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await
+                    client.GetAsync("http://localhost:22887/api/Books");
+                //IEnumerable<Book> res = await
+                //    response.Content.ReadFromJsonAsync<IEnumerable<Book>>();
+                string res = await
+                    response.Content.ReadAsStringAsync();
+                return res;
+            }
+        }
+        public async Task<string> Get(string isbn)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await
+                       client.GetAsync("http://localhost:22887/api/Books/"+isbn);
+                string res = await
+                    response.Content.ReadAsStringAsync();
+                return res;
+            }
+        }
+        public async Task<Book> Post(string bookJson)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                JsonContent bookToPost = JsonContent.Create(bookJson);
+                HttpResponseMessage response = await
+                    client.PostAsync("http://localhost:22887/api/Books/", bookToPost);
+                Book res = await
+                    response.Content.ReadFromJsonAsync<Book>();
+                return res;
+            }
+        }
+        public async Task<Book> Put(string bookJson)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                JsonContent bookToPut = JsonContent.Create(bookJson);
+                Book updatedBook = JsonConvert.DeserializeObject<Book>(bookJson);
+                HttpResponseMessage response = await
+                    client.PutAsync("http://localhost:22887/api/Books/" + updatedBook.ISBN13, bookToPut);
+                Book res = await
+                    response.Content.ReadFromJsonAsync<Book>();
+                return res;
+            }
+        }
+        public async void Remove(string isbn)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await
+                    client.DeleteAsync("http://localhost:22887/api/Books/" + isbn);
+
+            }
+        }
+    }
+}
